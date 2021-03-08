2dd30d74d39629b15cd6622d0a5da60ab8624e34
Nigel Nelson <nigel.mark.nelson@gmail.com>
2021-03-08 01:34:04 -0700

    Room object

--- src/server.js
+++ src/server.js
@@ -2,45 +2,31 @@ import WebSocket from 'ws';
 import { Player } from './model.js'
 import { MAP_TILES } from './map.js';
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 12000 })
-
 const MAX_PLAYERS = 256;
-const availableIDs = Array(MAX_PLAYERS).fill().map((_,i) => i);
 
-const players = new Set();
-const hasMouse = {};
-const scores = {};
-const names = {};
-
-let mousex = 54;
-let mousey = 41;
-
-function broadcast(packet) {
-  if (typeof packet === 'string') {
-    console.log(packet)
-  }
-  for (const socket of wss.clients) {
-    socket.send(packet)
-  }
-}
-function broadcastRoom(sender, packet) {
-  if (typeof packet === 'string') {
-    console.log(packet)
-  }
-  for (const other of players) {
-    if (sender.sameRoomAs(other)) {
-      other.socket.send(packet)
-    }
-  }
-}
-
-const padMessages = {
+const PAD_MESSAGES = {
   100: isDog => isDog ? 'Woof Woof Woof!' : "I'm in the mush room!",
   101: isDog => isDog ? 'Woof Woof Woof!' : "I'm by the fountain!",
   102: isDog => isDog ? 'Woof Woof Woof!' : "I found treasure!",
   103: isDog => isDog ? 'Woof Woof Woof!' : "Meet me in the alley!",
   104: isDog => isDog ? 'Woof! Woof Woof Woof!' : "Meow Meow Meow!",
 }
+
+class Room {
+  constructor () {
+    this.availableIDs = Array(MAX_PLAYERS).fill().map((_,i) => i);
+    this.players = new Set();
+    this.hasMouse = {};
+    this.scores = {};
+    this.names = {};
+    this.mousex = 54;
+    this.mousey = 41;
+  }
+}
+
+const wss = new WebSocket.Server({ port: process.env.PORT || 12000 })
+
+const room = new Room();
 
 wss.on('connection', function connection(ws, request) {
   const name = new URL(request.url, 'https://example.org').searchParams.get('name');
@@ -52,28 +38,52 @@ wss.on('connection', function connection(ws, request) {
     ws.close(4400, 'invalid name');
     return;
   }
-  for (const other of players) {
-    if (names[other.id] === name) {
+  room.accept(ws, name);
+});
+
+Room.prototype.broadcast = function broadcast(packet) {
+  if (typeof packet === 'string') {
+    console.log(packet)
+  }
+  for (const player of this.players) {
+    player.socket.send(packet)
+  }
+}
+
+Room.prototype.broadcastRoom = function broadcastRoom(sender, packet) {
+  if (typeof packet === 'string') {
+    console.log(packet)
+  }
+  for (const other of this.players) {
+    if (sender.sameRoomAs(other)) {
+      other.socket.send(packet)
+    }
+  }
+}
+
+Room.prototype.accept = function accept (ws, name) {
+  for (const other of this.players) {
+    if (this.names[other.id] === name) {
       ws.close(4400, 'That name is already taken');
       return;
     }
   }
-  if (players.size === MAX_PLAYERS) {
+  if (this.players.size === MAX_PLAYERS) {
     ws.close(4509, "Too many players online")
     return;
   }
 
-  const player = new Player(availableIDs.pop());
+  const player = new Player(this.availableIDs.pop());
   player.socket = ws;
-  players.add(player);
-  hasMouse[player.id] = false;
-  scores[player.id] = 0;
-  names[player.id] = name;
+  this.players.add(player);
+  this.hasMouse[player.id] = false;
+  this.scores[player.id] = 0;
+  this.names[player.id] = name;
 
-  function broadcastSelfToAllPlayers() {
+  const broadcastSelfToAllPlayers = () => {
     const buffer = Buffer.alloc(4);
     buffer.writeInt32BE(player.toInt32())
-    broadcast(buffer);
+    this.broadcast(buffer);
   }
   
   ws.on('pong', () => ws.isAlive = true)
@@ -92,71 +102,72 @@ wss.on('connection', function connection(ws, request) {
           return;
         }
         const oldState = player.copy();
-        const moved = player.move(data[2], players)
+        const moved = player.move(data[2], this.players)
         if (!moved) {
           ws.send('invalid invalid')
           return;
         } else if (!player.sameRoomAs(oldState)) {
-          broadcastRoom(oldState, `move-message [ ${names[player.id].toLocaleUpperCase()} LEFT ${oldState.roomName()} ]`)
-          broadcastRoom(player, `move-message [ ${names[player.id].toLocaleUpperCase()} ENTERED ${player.roomName()} ]`)
+          this.broadcastRoom(oldState, `move-message [ ${this.names[player.id].toLocaleUpperCase()} LEFT ${oldState.roomName()} ]`)
+          this.broadcastRoom(player, `move-message [ ${this.names[player.id].toLocaleUpperCase()} ENTERED ${player.roomName()} ]`)
         }
         const tileAt = MAP_TILES[player.y][player.x];
-        if (player.x === mousex && player.y === mousey && !hasMouse[player.id]) {
+        if (player.x === this.mousex && player.y === this.mousey && !this.hasMouse[player.id]) {
           do {
-            mousex = 20 + Math.random() * 60 | 0;
-            mousey = 12 + Math.random() * 36 | 0;
-          } while (MAP_TILES[mousey][mousex] !== 0)
-          broadcast(`mouse [${mousex},${mousey}]`)
-          hasMouse[player.id] = true;
+            this.mousex = 20 + Math.random() * 60 | 0;
+            this.mousey = 12 + Math.random() * 36 | 0;
+          } while (MAP_TILES[this.mousey][this.mousex] !== 0)
+          this.broadcast(`mouse [${this.mousex},${this.mousey}]`)
+          this.hasMouse[player.id] = true;
           ws.send('hasmouse true')
-        } else if (player.isAtDoorstep() && hasMouse[player.id]) {
-          hasMouse[player.id] = false;
-          scores[player.id]++;
+        } else if (player.isAtDoorstep() && this.hasMouse[player.id]) {
+          this.hasMouse[player.id] = false;
+          this.scores[player.id]++;
           ws.send('hasmouse false')
-          broadcast(`score {"id":${player.id},"score":${scores[player.id]},"cat":true}`)
-        } else if (padMessages[tileAt]) {
-          const msg = `-= BROADCAST FROM ${names[player.id].toLocaleUpperCase()}: ${padMessages[tileAt](player.isDog)} =-`
-          broadcast(`pad-message ${msg}`)
+          this.broadcast(`score {"id":${player.id},"score":${this.scores[player.id]},"cat":true}`)
+        } else if (PAD_MESSAGES[tileAt]) {
+          const msg = `-= BROADCAST FROM ${this.names[player.id].toLocaleUpperCase()}: ${PAD_MESSAGES[tileAt](player.isDog)} =-`
+          this.broadcast(`pad-message ${msg}`)
         }
         broadcastSelfToAllPlayers();
       } catch (err) {
+        console.trace(err)
         ws.close(4400, err.message)
       }
     } else {
       if (data.length > 50) {
         return;
       }
-      broadcastRoom(player, `${player.id} ${data}`);
+      this.broadcastRoom(player, `${player.id} ${data}`);
       player.applyChatMessage(data);
     }
   })
 
   ws.once('close', () => {
-    players.delete(player);
-    availableIDs.push(player.id)
+    this.players.delete(player);
+    this.availableIDs.push(player.id)
     const buffer = Buffer.alloc(4);
     buffer.writeInt32BE(player.toDeletedInt32())
-    broadcast(buffer)
-    broadcast(`join-message [ ${names[player.id].toLocaleUpperCase()} LEFT THE GAME ]`)
+    this.broadcast(buffer)
+    this.broadcast(`join-message [ ${this.names[player.id].toLocaleUpperCase()} LEFT THE GAME ]`)
   });
 
-  const firstPayload = Buffer.alloc(players.size * 4)
-  for (let i = 0, playerIterator = players.values(); i < players.size; ++i) {
+  const firstPayload = Buffer.alloc(this.players.size * 4)
+  for (let i = 0, playerIterator = this.players.values(); i < this.players.size; ++i) {
     firstPayload.writeInt32BE(playerIterator.next().value.toInt32(), i * 4);
   }
   ws.send(`id ${player.id}`)
-  ws.send(`names ${JSON.stringify(names)}`);
+  ws.send(`names ${JSON.stringify(this.names)}`);
   ws.send(firstPayload)
-  ws.send(`mouse [${mousex},${mousey}]`)
-  broadcast(`join-message [ ${names[player.id].toLocaleUpperCase()} ENTERED THE GAME ]`)
-  broadcast(`names ${JSON.stringify({ [player.id]: name })}`);
+  ws.send(`mouse [${this.mousex},${this.mousey}]`)
+  this.broadcast(`join-message [ ${this.names[player.id].toLocaleUpperCase()} ENTERED THE GAME ]`)
+  this.broadcast(`names ${JSON.stringify({ [player.id]: name })}`);
 
   const buffer = Buffer.alloc(4);
   buffer.writeInt32BE(player.toInt32())
-  for (const socket of wss.clients) {
-    if (socket !== ws) socket.send(buffer)
+  for (const other of this.players) {
+    if (player !== other) other.socket.send(buffer)
   }
-});
+};
 
 setInterval(() => {
   for (const socket of wss.clients) {
