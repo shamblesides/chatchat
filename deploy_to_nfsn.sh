ae3e547d4ba33a9c0fd22a28dde9586a5f698cb0
Nigel Nelson <nigel.mark.nelson@gmail.com>
2022-05-20 22:58:31 -0600

    script for deploying the backend

---
+++ deploy_to_nfsn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script just helps me deploy ChatChat's backend to my
+# current host, nearlyfreespeech.net.
+# It may or may not be of any use to you!
+
+set -e
+
+REMOTE=`cat .remote-host`
+
+echo 'Running rsync'
+rsync -arzP --delete --exclude=src/graphics --exclude=src/sounds package* src $REMOTE:/home/public
+
+echo 'Running npm install'
+ssh $REMOTE "cd /home/public && npm install"
+
+echo "SIGKILL daemon process"
+ssh $REMOTE nfsn signal-daemon runserver KILL
